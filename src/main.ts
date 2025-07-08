eaedebb845d8e7cd1eec3349e9d27af76f8c9ccd
Maksym Rybalko <maxymrybalko2005@gmail.com>
2025-07-09 00:08:22 +0300

    Added animation lesson

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import './style.css';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import Stats from 'three/addons/libs/stats.module.js';
-import { GUI } from 'dat.gui';
 
 const scene = new THREE.Scene();
-scene.add(new THREE.GridHelper());
 
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -12,8 +11,7 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000,
 );
-camera.position.set(0, 2, 3);
-camera.lookAt(0, 0.5, 0);
+camera.position.z = 1.5;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -25,45 +23,26 @@ window.addEventListener('resize', () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
+new OrbitControls(camera, renderer.domElement);
+
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshNormalMaterial({ wireframe: true });
 
 const cube = new THREE.Mesh(geometry, material);
-cube.position.y = 0.5;
 scene.add(cube);
 
 const stats = new Stats();
 document.body.appendChild(stats.dom);
 
-const gui = new GUI();
-const cubeFolder = gui.addFolder('Cube');
-cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2);
-cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2);
-cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2);
-cubeFolder.open();
-
-const cameraFolder = gui.addFolder('Camera');
-cameraFolder.add(camera.position, 'x', -10, 10);
-cameraFolder.add(camera.position, 'y', -10, 10);
-cameraFolder.add(camera.position, 'z', -10, 10);
-cameraFolder.add(camera, 'fov', 0, 180, 0.01).onChange(() => {
-  camera.updateProjectionMatrix();
-});
-cameraFolder.add(camera, 'aspect', 0.00001, 10).onChange(() => {
-  camera.updateProjectionMatrix();
-});
-cameraFolder.add(camera, 'near', 0.01, 10).onChange(() => {
-  camera.updateProjectionMatrix();
-});
-cameraFolder.add(camera, 'far', 0.01, 10).onChange(() => {
-  camera.updateProjectionMatrix();
-});
-cameraFolder.open();
-
+const clock = new THREE.Clock();
+let delta;
 const animate = () => {
   requestAnimationFrame(animate);
 
-  camera.lookAt(0, 0.5, 0);
+  delta = clock.getDelta();
+
+  cube.rotation.x += delta;
+  cube.rotation.y += delta;
 
   renderer.render(scene, camera);
   stats.update();
